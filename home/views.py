220c3451479c0f38140ea6608cee85a51094434a
LordButley <joshjwwilson@gmail.com>
2022-10-28 12:49:50 +0000

    create base.html, index.html, landing.html, header_landing.html, header_shop.html and wire up

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def landing(request):
+    """ A view to return the landing page """
+    
+    return render(request, 'home/landing.html')
+
+def index(request):
+    """ A view to return the index page """
+    
+    return render(request, 'home/index.html')
