3e68d842dbf4b041f593adf6ff402a34f23b2b49
LordButley <joshjwwilson@gmail.com>
2022-11-09 09:16:05 +0000

    add fields to product model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=254)
     readable_name = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -18,9 +22,12 @@ class Product(models.Model):
 
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.PositiveIntegerField(blank= False, default=0)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+    
 
 
     def __str__(self):
@@ -28,6 +35,9 @@ class Product(models.Model):
 
 class Product_Category(models.Model):
    
+    class Meta:
+        verbose_name_plural = "Product_Category"
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
 
