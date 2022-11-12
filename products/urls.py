504bec94e4dd8df700219e09f39e127333c0c5e2
LordButley <joshjwwilson@gmail.com>
2022-11-12 15:42:42 +0000

    create view_product page

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.shop, name='shop'),
     path('products/', views.all_products, name='all_products'),
+    path('<product_id>', views.view_product, name='view_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Product_Category, Category
 from datetime import datetime, timedelta
 
@@ -39,4 +39,26 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def view_product(request, product_id):
+    """ A view to return the all products, including sorting queries"""
+
+    # product_categories = Product_Category.objects.all()
+    # products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
+    # if request.GET:
+    #     if 'category' in request.GET:
+    #         categories = request.GET['category'].split(',')
+    #         if 'new' in categories:
+                # product_categories = "Check"
+            #     product_categories = product_categories.exclude(product__date_added__lt = (datetime.today() - timedelta(weeks=4)))
+            # else:
+            #     product_categories = product_categories.filter(category__name__in=categories)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/view_product.html', context)
+
 
