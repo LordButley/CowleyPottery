751bdc861a6255102aa986c9265331b480d248ca
LordButley <joshjwwilson@gmail.com>
2022-11-15 10:10:59 +0000

    add add_to_bag view and form

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -10,3 +10,19 @@ def view_bag(request):
     }
 
     return render(request, 'bag/bag.html', context)
+
+
+def add_to_bag(request, product_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
