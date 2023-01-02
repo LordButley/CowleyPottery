7323cec47d9c42df42e4bb59ca36eac1baba5478
LordButley <joshjwwilson@gmail.com>
2023-01-02 14:56:23 +0000

    testing deploying of images with h1 tag on index page

--- cowley_pottery/settings.py
+++ cowley_pottery/settings.py
@@ -204,6 +204,10 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
+# else:
+
+#     MEDIA_URL = "media/"
+
 #STRIPE
 
 FREE_DELIVERY_THRESHOLD = 100
