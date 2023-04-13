1fc53501ce05ece84684f735df2a7e055edf9ef4
LordButley <joshjwwilson@gmail.com>
2023-04-13 16:24:20 +0000

    add custom domains 2

--- cowley_pottery/settings.py
+++ cowley_pottery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = False
-ALLOWED_HOSTS = ['cowley-pottery.herokuapp.com', 'localhost', 'www.cowleypottery.co.uk']
+ALLOWED_HOSTS = ['cowley-pottery.herokuapp.com', 'localhost', 'www.cowleypottery.co.uk', 'www.cowleypottery.com', 'https://cowleypottery.com']
 
 
 # Application definition
