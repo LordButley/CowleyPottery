0560f8f79678540a3ef430d7c74d84c0ef17124a
LordButley <joshjwwilson@gmail.com>
2023-04-25 11:51:44 +0000

    edit email template and recipient list

--- cowley_pottery/settings.py
+++ cowley_pottery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['cowley-pottery.herokuapp.com', 'localhost', 'www.cowleypottery.co.uk', 'www.cowleypottery.com', 'https://www.cowleypottery.com']
 
 
